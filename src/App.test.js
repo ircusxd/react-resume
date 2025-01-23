ec5239656e85d77fad6f26f0a3d08c65b1172ea2
Hugo GarcíaR Rubio <hugo.garciar@informatica.cifpaviles.pa>
2025-01-23 18:23:12 +0100

    Tests y CI Workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from "./data.json";
 
-test('renders learn react link', () => {
+test('aplicar tema por defecto', () => {
+  window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+      matches: query === '(prefers-color-scheme: dark)',
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const htmlElement = document.documentElement;
+  const appliedTheme = htmlElement.getAttribute('data-theme');
+
+  expect(['light', 'dark']).toContain(appliedTheme);
+});
+
+test('renderiza toda la informacion', () => {
+  render(<App />);
+
+  expect(screen.getByText(data.info.name)).toBeInTheDocument();
+  expect(screen.getByText(data.experience[0].company)).toBeInTheDocument();
+  expect(screen.getByText(data.skills[0].name)).toBeInTheDocument();
+  expect(screen.getByText(data.education[0].degree)).toBeInTheDocument();
 });
